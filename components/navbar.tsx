509fe429749b93d1cbb1ebde7c9aef53a0135c81
Ridwan Halim <91854659+ridwaanhall@users.noreply.github.com>
2025-01-19 17:33:31 +0700

    Update: remove unused TwitterIcon import from navbar component

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,5 +1,5 @@
 import { ModeToggle } from "@/components/theme-toggle";
-import { GithubIcon, TwitterIcon, CommandIcon } from "lucide-react";
+import { GithubIcon, CommandIcon } from "lucide-react";
 import Link from "next/link";
 import { buttonVariants } from "./ui/button";
 import Search from "./search";
